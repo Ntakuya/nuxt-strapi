1beef3002168ffecfc64065278a849dbdd2a2fe5
NTakuya <nagahama.takuya@outlook.jp>
2020-07-26 20:50:07 +0900

    WIP

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed800c8abfe77dbc567cef2007d64f23'),
+    },
+  },
+});
