fea03f117b13357e4af85fddbb826914298ada3b
NTakuya <nagahama.takuya@outlook.jp>
2020-07-26 16:44:28 +0900

    WIP

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7f96379cf95e822fa108fed61d65cba'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8fe553c0-fa6b-4c75-aeec-7249e9765148'
+};
